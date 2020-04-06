2169f0fa6c8c552ae8feb992955e75bcce2872e3
morticius <rostislav.khantil@gmail.com>
2020-04-06 10:32:14 +0300

    Created view for admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Categories 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Materials 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Visitors 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Today 0</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create category</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">First category</h4>
+                    <p class="list-group-item-text">
+                        Counted materials
+                    </p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create material</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Last added material</h4>
+                    <p class="list-group-item-text">
+                        Category
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ tests/Http/Controllers/Auth/ConfirmPasswordControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Http\Controllers\Auth;
+
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+
+class ConfirmPasswordControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test__construct()
+    {
+
+    }
+}
